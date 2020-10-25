c0a0ccc540b1d92c836e32715f6cb0ae02b84d13
“vinsantdeveloper” <suttipongworks@gmail.com>
2020-10-25 17:07:46 +0900

    finished first assignment

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert('This works!');
+let currentResult = 0;
+
+currentResult += 10;
+
+let calculationDescription = 'Holy shit!!';
+
+outputResult(currentResult, calculationDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 10;
+let result;
+
+result = 18 + userInput;
+
+result *= 20;
+result -= 3;
+result /= userInput;
+
+alert(userInput);
+alert(result);
